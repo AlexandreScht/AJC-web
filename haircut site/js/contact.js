42a3782be15722554d944e17c64b9d6ea30bb41c
SCHECHT <92469225+AlexandreScht@users.noreply.github.com>
2022-01-12 14:07:04 +0100

    Delete haircut site directory

--- haircut site/js/contact.js
+++
@@ -1,7 +0,0 @@
- function avis(){
-    document.getElementById("devant").style.zIndex = "0";
-    document.getElementById("devant").style.WebkitTransitionDuration="1s";
-    document.getElementById("devant").style.webkitTransform = 'rotateY(-180deg)';
-    document.getElementById("derriere").style.WebkitTransitionDuration="1s";
-    document.getElementById("derriere").style.webkitTransform = 'rotateY(0deg)';
-}
